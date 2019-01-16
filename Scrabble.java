b0a92185b4e4b0b90d725976de4e8e30fb3c1595
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-01-16 14:35:53 +0530

    Update Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -1,19 +1,18 @@
+import java.io.FileNotFoundException;
 public class Scrabble
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         WordGame wordgame = new WordGame("movies.txt");
-        System.out.println("Welcome to Guess the Movie!");
-        System.out.println("The rules are simple, the computer randomly picks a movie title, and shows you how " + "many " + "letters it's made up of.");
+        System.out.printf("The rules are simple, the computer randomly picks a movie title, and shows you how many letters it's made up of.%n");
         System.out.println("Your goal is to try to figure out the movie by guessing one letter at a time.");
-        System.out.println("If a letter is indeed in the title the computer will reveal its correct position in" + " the " + "word, if not, you lose a point.");
+        System.out.printf("If a letter is indeed in the title the computer will reveal its correct position in the word, if not, you lose a point.%n");
         System.out.println("If you lose 10 points, game over!");
         System.out.println("Let's start!");
-        System.out.println("The movie title has " + wordgame.getMovieTitle().length() + " characters (including spaces " + "and punctuation).");
+        System.out.printf("The movie title has %d characters (including spaces and punctuation).%n", wordgame.getMovieTitle().length());
         while(!wordgame.gameEnded())
         {
             System.out.println("You are guessing:" + wordgame.getHiddenMovieTitle());
-            System.out.println("You have guessed (" + wordgame.getWrongLetters().length()/2 + ") wrong letters:" + wordgame.getWrongLetters());
+            System.out.printf("You have guessed (%d) wrong letters:%s%n", wordgame.getWrongLetters().length() / 2, wordgame.getWrongLetters());
             wordgame.guessLetter();
         }
         if(wordgame.WonGame())
@@ -23,9 +22,9 @@ public class Scrabble
         }
         else
         {
-            System.out.println("You have guessed (" + wordgame.getWrongLetters().length()/2 + ") wrong letters:" + wordgame.getWrongLetters());
+            System.out.printf("You have guessed (%d) wrong letters:%s%n", wordgame.getWrongLetters().length() / 2, wordgame.getWrongLetters());
             System.out.println("You lost!");
-            System.out.println("The movie title was " + wordgame.getMovieTitle());
+            System.out.printf("The movie title was %s%n", wordgame.getMovieTitle());
             System.out.println("Better luck next time.");
         }
     }
